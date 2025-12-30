00ed59c628f1e3104f6dcdc8054d0e1e514700bd
Mutasim Sazedin <msazedin@lakeheadu.ca>
2025-12-30 16:05:42 -0500

    feat: restore recipe generation flow

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if upload_image:
                         {
                         'type': 'input_text',
                         'text': 'Identify all visible food ingredients in this image'
-                        'Return ONLY in valid JSON format and NOTHING ELSE(VERY STRICT):' '{"ingredients": ["item1", "item2"]}'
+                        'Return ONLY in valid JSON format and NOTHING ELSE. Make sure to return JUST JSON and follow the given format:' '{"ingredients": ["item1", "item2"]}'
                         },
                         {
                             'type': 'input_image',
@@ -62,3 +62,61 @@ if upload_image:
 
     st.subheader('Ingredients Detected')
     st.write(ingredients)
+
+    
+    st.divider()
+
+    generate = st.button("Generate Recipes")
+
+    if generate:
+        recipe_prompt = f"""
+        You are a professional chef AI.
+        Using ONLY the following ingredients {ingredients}
+        Suggest 3 different recipes.
+
+        Rules:
+        -Do Not invent ingredients
+        -Use simple cooking steps
+        -Assume basic pantry items (salt, water, oil)
+
+        You MUST return ONLY valid JSON.
+        NO markdown.
+        NO explanations.
+        NO extra text.
+
+        Return ONLY valid JSON in this format(STRICT):
+        {{
+        'recipes': [
+            {{
+                "name": "Recipe Name",
+                "ingredients_used": ["ingredient1", "ingredient2"],
+                "steps": ["step1", "step2", "step3"]
+            }}
+        ]
+        }}
+        """
+
+        with st.spinner("Generating Recipes..."):
+            recipe_response = client.responses.create(
+                model = 'gpt-4.1-mini',
+                input = recipe_prompt
+            )
+
+            recipe_output = recipe_response.output[0].content[0].text
+
+            try:
+                recipe_data = json.loads(recipe_output)
+            except json.JSONDecodeError:
+                st.error('Failed to parse recipes')
+                st.stop()
+
+            st.subheader('Recipe Recommendations')
+
+            for recipe in recipe_data["recipes"]:
+                st.markdown(f"# {recipe["name"]}")
+                st.markdown("Ingredients Used:")
+                st.write(recipe["ingredients_used"])
+                st.markdown("***Steps***")
+
+                for step in recipe["steps"]:
+                    st.write(f'- {step}')
